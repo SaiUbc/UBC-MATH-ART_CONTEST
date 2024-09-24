e90b301712847ee2e8389b15122ecd9b508f47c8
SaiUbc <psilegends@gmail.com>
2024-09-24 15:25:30 -0700

    ✨ added basic ascii art on terminal function

---
+++ src/ascii_art_basic.py
@@ -0,0 +1,42 @@
+from PIL import Image
+
+# Define ASCII characters from dark to light
+ASCII_CHARS = "ΣΨΔΘ∑∫√∞≈≠≤≥÷×∝∀∃∅∈∉∋∌∑∏∐∑∫∬∭∮∯∰∱∲∳∴∵∶∷∸∹∺∻∼∽∾∿≀≁≂≃≄≅≆≇≈≉≊≋≌≍≎≏≐≑≒≓≔≕≖≗≘≙≚≛≜≝≞≟≠≡≢≣≤≥≦≧≨≩≪≫≬≭≮≯≰≱≲≳≴≵≶≷≸≹≺≻≼≽≾≿⊀⊁⊂⊃⊄⊅⊆⊇⊈⊉⊊⊋⊌⊍⊎⊏⊐⊑⊒⊓⊔⊕⊖⊗⊘⊙⊚⊛⊜⊝⊞⊟⊠⊡⊢⊣⊤⊥⊦⊧⊨⊩⊪⊫⊬⊭⊮⊯⊰⊱⊲⊳⊴⊵⊶⊷⊸⊹⊺⊻⊼⊽⊾⊿⋀⋁⋂⋃⋄⋅⋆⋇⋈⋉⋊⋋⋌⋍⋎⋏⋐⋑⋒⋓⋔⋕⋖⋗⋘⋙⋚⋛⋜⋝⋞⋟⋠⋡⋢⋣⋤⋥⋦⋧⋨⋩⋪⋫⋬⋭⋮⋯⋰⋱⋲⋳⋴⋵⋶⋷⋸⋹⋺⋻⋼⋽⋾⋿"
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    aspect_ratio = height / width
+    new_height = int(aspect_ratio * new_width * 0.55)  # Adjusting aspect ratio
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
+
+def grayscale_image(image):
+    return image.convert("L")
+
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    ascii_str = "".join([ASCII_CHARS[pixel // 32] for pixel in pixels])
+    return ascii_str
+
+def image_to_ascii(image_path, new_width=100):
+    try:
+        image = Image.open(image_path)
+    except Exception as e:
+        print(e)
+        return
+
+    image = resize_image(image, new_width)
+    grayscale = grayscale_image(image)
+    
+    ascii_str = pixels_to_ascii(grayscale)
+    
+    img_width = grayscale.width
+    ascii_str_len = len(ascii_str)
+    ascii_img = "\n".join([ascii_str[index:(index + img_width)] for index in range(0, ascii_str_len, img_width)])
+    
+    return ascii_img
+
+
+image_path = "pics/spiderman.png"
+ascii_art = image_to_ascii(image_path)
+print(ascii_art)
